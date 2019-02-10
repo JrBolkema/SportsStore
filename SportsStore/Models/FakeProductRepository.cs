2523d49e1497077763ffde2d7ee358c6d5153615
Jon Bolkema <jrbolkema@gmail.com>
2019-02-10 15:58:05 -0700

    Up until page  205

---
+++ SportsStore/Models/FakeProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+	public class FakeProductRepository : IProductRepository
+	{
+		public IQueryable<Product> Products => new List<Product>
+		{
+			new Product {Name = "Football",Price = 25 },
+			new Product {Name = "Football",Price = 25 },
+			new Product {Name = "Football",Price = 25 }
+
+		}.AsQueryable<Product>();
+	}
+}
